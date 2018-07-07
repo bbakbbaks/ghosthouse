c448673db559c69b8670da5df932fc20f6f0df0c
bbakbbaks <38639675+bbakbbaks@users.noreply.github.com>
2018-07-07 13:24:31 +0900

    test1

---
+++ ghosthousetest_2/Assets/Script/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public GameObject ghost1;
+
+    static GameManager m_cInstance;
+
+    static public GameManager GetInstance()
+    {
+        return m_cInstance;
+    }
+
+    // Use this for initialization
+    void Start () {
+        m_cInstance = this;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        PdGhost();
+	}
+
+    public void PdGhost()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitinfo = new RaycastHit();
+        if (Physics.Raycast(ray, out hitinfo, 100.0f, 1 << LayerMask.NameToLayer("Default")))
+        {
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                GameObject ghost_1 = Instantiate(ghost1, hitinfo.point, Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ ghosthousetest_2/Assets/Script/camera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour {
+    public float m_fSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CameraMove();
+	}
+
+    public void CameraMove()
+    {
+        float fVertical = Input.GetAxis("Vertical");
+        float fHorizontal = Input.GetAxis("Horizontal");
+
+        if (Mathf.Abs(fVertical) > 0 || Mathf.Abs(fHorizontal) > 0)
+        {
+            float fYTranslation = fVertical * m_fSpeed;
+            float fXTranslation = fHorizontal * m_fSpeed;
+            fXTranslation *= Time.deltaTime;
+            fYTranslation *= Time.deltaTime;
+            transform.Translate(fXTranslation, fYTranslation, 0);
+        }
+
+        Vector3 limitmap;
+        limitmap.x = Mathf.Clamp(transform.position.x, -22, (float)16.6);
+        limitmap.y = Mathf.Clamp(transform.position.y, (float)-23.2, (float)12.3);
+        limitmap.z = Mathf.Clamp(transform.position.z, -10, 10);
+        transform.position = limitmap;
+
+
+    }
+
+}
